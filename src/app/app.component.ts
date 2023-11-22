a32623c9747fefeff5687baf2290ac0ca834346c
Kevin Rivera <shimpo11.kr@gmail.com>
2023-11-22 17:34:18 -0500

    Add input and meta tags to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FrontTI';
+export class AppComponent implements OnInit {
+
+  constructor(private meta: Meta) { }
+
+  title = 'TECNOLOGIA WEB';
+
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'description', content: 'Explora nuestros servicios tecnológicos en nuestro eCommerce. Ofrecemos soluciones innovadoras en desarrollo web, diseño UX/UI, análisis de datos y más. Optimiza tu presencia online con nuestros servicios especializados.' });
+  }
+
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
+
+  isSideNavCollapsed = false;
+  onToggleSideNav(data: SideNavToggle): void {
+    this.isSideNavCollapsed = data.collapsed;
+    this.screenWidth = data.screenWidth;
+  }
 }
