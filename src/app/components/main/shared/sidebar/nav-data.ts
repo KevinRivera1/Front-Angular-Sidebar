21aad75631395e7d967230d788bfa76c5dbd7f4e
Kevin Rivera <shimpo11.kr@gmail.com>
2023-11-22 17:37:13 -0500

    Add sidebar navigation data

---
+++ src/app/components/main/shared/sidebar/nav-data.ts
@@ -0,0 +1,12 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard/',
+        icon: 'fas fa-home',
+        label: 'Dashboard'
+    },
+    {
+        routeLink: 'dashboard/products',
+        icon: 'fas fa-box',
+        label: 'Products'
+    }
+];
