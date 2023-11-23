df3fbc3489a1fffe03c4a50b6e86ecceefa35e1f
Kevin Rivera <shimpo11.kr@gmail.com>
2023-11-23 17:27:29 -0500

    Refactor menu component to include responsive behavior

--- src/app/components/main/shared/menu/menu.component.ts
+++ src/app/components/main/shared/menu/menu.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  
 
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
+
+  canShowSearchOverlay = false;
+
+  constructor() { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkCanShowSearchOverlay(window.innerWidth);
+  }
+
+  ngOnInit(): void {
+    this.checkCanShowSearchOverlay(window.innerWidth);
+  }
+
+  getHeadClass(): string {
+    let styleClass = '';
+    if (this.collapsed && this.screenWidth > 768) {
+      styleClass = 'head-trimmed';
+    } else {
+      styleClass = 'head-md-screen';
+    }
+    return styleClass;
+  }
+
+  checkCanShowSearchOverlay(innerWidht: number): void {
+    if (innerWidht > 845) {
+      this.canShowSearchOverlay = true;
+    } else {
+      this.canShowSearchOverlay = false;
+    }
+  }
 }
