a0ae63d566442f3c923676d6f9cabedc48f9d1d7
Kevin Rivera <shimpo11.kr@gmail.com>
2023-11-23 15:39:44 -0500

    Refactor sidebar navigation data structure

--- src/app/components/main/shared/sidebar/nav-data.ts
+++ src/app/components/main/shared/sidebar/nav-data.ts
@@ -1,4 +1,6 @@
-export const navbarData = [
+import { INavbarData } from "./helper";
+
+export const navbarData: INavbarData[] = [
     {
         routeLink: 'dashboard/',
         icon: 'fas fa-home',
@@ -7,6 +9,46 @@ export const navbarData = [
     {
         routeLink: 'dashboard/products',
         icon: 'fas fa-box',
-        label: 'Products'
+        label: 'Products',
+        items: [
+            {
+                routeLink: 'list/products',
+                label: 'All Products'
+            },
+            {
+                routeLink: 'add/product',
+                label: 'Add Product',
+            },
+            {
+                routeLink: 'list/categories',
+                label: 'Categories'
+            },
+            {
+                routeLink: 'add/category',
+                label: 'Add Category',
+                items: [
+                    {
+                        routeLink: 'list/categories',
+                        label: 'All Categories'
+                    },
+                    {
+                        routeLink: 'add/category',
+                        label: 'Add Category',
+                        items:[
+                            {
+                                routeLink: 'list/categories',
+                                label: 'All Categories'
+                            },
+                            {
+                                routeLink: 'add/category',
+                                label: 'Add Category',
+                            }
+                        
+                        ]
+                    }
+                
+                ]
+            }
+        ]
     }
 ];
