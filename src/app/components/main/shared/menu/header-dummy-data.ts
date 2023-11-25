de21ee7772d4cb947f9308952a23ea0f6425291e
Kevin Rivera <shimpo11.kr@gmail.com>
2023-11-25 16:43:37 -0500

    Add header dummy data for user menu options

---
+++ src/app/components/main/shared/menu/header-dummy-data.ts
@@ -0,0 +1,15 @@
+// menu de opciones del perfil de usuario
+export const userItems = [
+    {
+        icon: 'fa fa-user',
+        label: 'Perfil'
+    },
+    {
+        icon: 'fa fa-cog',
+        label: 'Configuración'
+    },
+    {
+        icon: 'fa fa-sign-out',
+        label: 'Cerrar sesión'
+    }
+]
